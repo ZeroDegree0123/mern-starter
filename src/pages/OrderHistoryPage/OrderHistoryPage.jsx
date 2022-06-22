a436b3bcc1ed918abb6a4e34969a64d69aefcd29
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-06-22 15:54:25 -0700

    part 7

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,5 +1,15 @@
 import './OrderHistoryPage.css'
 
 export default function OrderHistoryPage() {
-    return <h1>hello Order History</h1>
+
+    const handleCheckToken = async (evt) => {
+        alert('click')
+    }
+
+    return( 
+        <>
+            <h1>hello Order History</h1>
+            <button onClick={handleCheckToken}>Check When My Login Expires</button>
+        </>
+    )
 }
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -35,3 +35,11 @@ export function getUser() {
 export function logOut() {
   localStorage.removeItem('token');
 }
+
+export function checkToken() {
+  // Just so that you don't forget how to use .then
+  return usersAPI.checkToken()
+    // checkToken returns a string, but let's 
+    // make it a Date object for more flexibility
+    .then(dateStr => new Date(dateStr));
+}
